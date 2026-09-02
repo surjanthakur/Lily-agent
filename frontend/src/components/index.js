27232191c4e0e0650dadc090e3eeed481c620f3a
surjanthakur <tsurjan506@gmail.com>
2026-09-02 17:12:38 +0530

    s

--- frontend/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Footer from './Footer.jsx'
-import NavBar from './Navbar.jsx'
-import { HowItWorks } from './Features.jsx'
-import FaqSection from './FAQ.jsx'
-import DemoSection from './DemoVideo.jsx'
-import Hero from './Hero.jsx'
-
-export { Footer, NavBar, HowItWorks, FaqSection, DemoSection, Hero }
