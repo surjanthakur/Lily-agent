7ddbfb9f98cbeae54f6affd1b3311f3700b15f73
surjanthakur <tsurjan506@gmail.com>
2026-09-06 11:31:03 +0530

    s

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from typing import Annotated, TypedDict
 
-from langgraph.graph import StateGraph
+from langgraph.graph import END, START, StateGraph
 
 
 # create state schema structure
@@ -28,6 +28,7 @@ def do_fallback(state: StateSchema):
     print(
         f"question: {state['question']} answer:{state['research']} attempts:{state['attempts']} is_good: {state['is_good']}"
     )
+    return END
 
 
 def do_answer(state: StateSchema):
@@ -35,6 +36,13 @@ def do_answer(state: StateSchema):
 
 
 def do_check(state: StateSchema):
+
+    if state["research"] and len(state["research"]) >= 10:
+        return {state["is_good"]: True}
+    return {state["is_good"]: False}
+
+
+def do_route(state: StateSchema):
 
     if state["attempts"] >= 3:
         return "fallback"
@@ -51,5 +59,17 @@ graph_builder.add_node("research", do_research)
 graph_builder.add_node("fallback", do_fallback)
 graph_builder.add_node("answer", do_answer)
 graph_builder.add_node("check", do_check)
+graph_builder.add_node("route", do_route)
+
+# define edges
+graph_builder.add_edge(START, "research")
+graph_builder.add_edge("research", "check")
+graph_builder.add_conditional_edges(
+    "check",
+    do_route,
+    {"fallback", "answer", "research"},
+)
+
+graph_builder.compile(graph_builder)
 # compile graph
 # invoke graph
