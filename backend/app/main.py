95027c5d06c6ec6e3e46d4293f513b717712ae95
surjanthakur <tsurjan506@gmail.com>
2026-09-09 16:05:56 +0530

    s

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,6 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from .db.databse import create_db_tables
-from .models.Conversations import Conversation  # noqa: F401
-from .models.Messages import Message  # noqa: F401
-from .models.Users import USER  # noqa: F401
 
 
 @asynccontextmanager
