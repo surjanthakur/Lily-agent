37e2c289bbfe7a64abec69f4d6a16b4c54946dfc
surjanthakur <tsurjan506@gmail.com>
2026-09-13 21:03:09 +0530

    s

--- backend/app/schemas/llm-schema.py
+++ backend/app/schemas/llm-schema.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, Field
 
 
-class LLMSchema(BaseModel):
-    user_input: str = Field(min_length=20, max_length=100)
+class LlmSchemaValidation(BaseModel):
+    user_input: str = Field(min_length=20, max_length=100, title="user req query")
+    model_name: str = Field(min_length=10, max_length=50, title="name of the model")
+    thinking_level: str = Field(
+        min_length=4,
+        max_length=10,
+        title="thinking level of the model",
+    )
