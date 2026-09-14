1fa4d02d9da53d2e59962ea064bca7bc507d0e60
surjanthakur <tsurjan506@gmail.com>
2026-09-14 18:04:45 +0530

    s

--- backend/app/core/langgraph/state_graph.py
+++ backend/app/core/langgraph/state_graph.py
@@ -1,7 +1,7 @@
 from operator import add
 from typing import Annotated, Literal, TypedDict
 
-from langgraph.graph import StateGraph, add_messages
+from langgraph.graph import StateGraph
 from pydantic import AnyUrl
 
 
@@ -16,14 +16,13 @@ class Source(TypedDict):
 
 class AgentState(TypedDict):
     topic: str
+    optimized_queries: list[str]
     found_resources: Annotated[
         list[Source], add
     ]  # founded resources with title, url, score only.
     scraped_resources: Annotated[
         list[Source], add
     ]  # after scrapted add metadata: title , url , score , level , type , description.
-    optimized_queries: list[str]
-    messages: Annotated[list, add_messages]  # store recent messages state
 
 
 GRAPH_BUILDER = StateGraph(AgentState)
