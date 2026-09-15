4f93e24f77be39abd3c5c16e3d4a9293de5d7180
surjanthakur <tsurjan506@gmail.com>
2026-09-15 19:36:49 +0530

    s

--- backend/app/routes/llm_router.py
+++ backend/app/routes/llm_router.py
@@ -5,6 +5,7 @@ from ..services.agent_services import call_langgraph_agent_workflow
 router = APIRouter(tags=["agent call"])
 
 
+# Route to get llm response
 @router.get("/ask", status_code=status.HTTP_200_OK)
 def get_agent_res_route(user_query: str):
     return {"response": call_langgraph_agent_workflow(user_query)}
