cb491ec133baf56612c8d262a44a6ad9065385f0
surjanthakur <tsurjan506@gmail.com>
2026-09-16 13:15:30 +0530

    s

--- backend/app/core/langgraph/state_graph.py
+++ backend/app/core/langgraph/state_graph.py
@@ -7,12 +7,7 @@ from langgraph.graph import StateGraph
 class AgentState(TypedDict):
     topic: str
     optimized_queries: list[str]
-    found_resources: Annotated[
-        list[dict], add
-    ]  # founded resources with title, url, score only.
-    scraped_resources: Annotated[
-        list[dict], add
-    ]  # after scrapted add metadata: title , url , score , level , type , description.
+    found_resources: Annotated[list[dict], add]
 
 
 GRAPH_BUILDER = StateGraph(AgentState)
