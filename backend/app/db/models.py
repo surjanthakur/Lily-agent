f458133a20bd24e8c05cfe901624ff288fab244a
surjanthakur <tsurjan506@gmail.com>
2026-09-17 21:19:38 +0530

    re structure model

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,9 +1,7 @@
 from datetime import datetime
-from enum import Enum
-from typing import Optional
 from uuid import UUID, uuid4
 
-from sqlmodel import Field, Relationship, SQLModel
+from sqlmodel import Field, SQLModel
 
 
 # user's table
@@ -36,69 +34,4 @@ class User(SQLModel, table=True):
     created_at: datetime = Field(
         default_factory=datetime.now,
         title="date and time the user was created",
-    )
-
-    Conversations: list["Conversation"] = Field(
-        Relationship(
-            back_populates="user",
-            cascade_delete=True,
-        )
-    )
-
-
-# user's conversation history
-class Conversation(SQLModel, table=True):
-    conversation_id: UUID = Field(
-        default_factory=uuid4,
-        primary_key=True,
-        title="unique id of the conversation",
-    )
-    user_id: UUID = Field(
-        foreign_key="user.user_id",
-        index=True,
-        ondelete="CASCADE",
-        title="user who owns the conversation",
-    )
-    created_at: datetime = Field(
-        default_factory=datetime.now,
-        title="date and time the conversation was created",
-    )
-
-    user: Optional["User"] = Field(Relationship(back_populates="conversations"))
-
-    messages: list["Message"] = Field(
-        Relationship(back_populates="conversation", cascade_delete=True)
-    )
-
-
-class MessageRole(str, Enum):
-    AGENT = "agent"
-    USER = "user"
-
-
-# conversation's history messages
-class Message(SQLModel, table=True):
-    message_id: UUID = Field(
-        default_factory=uuid4,
-        primary_key=True,
-        title="unique id of the message",
-    )
-    conversation_id: UUID = Field(
-        foreign_key="conversation.conversation_id",
-        index=True,
-        title="user who owns the conversation",
-        ondelete="CASCADE",
-    )
-    role: MessageRole = Field(
-        title="message sender role",
-    )
-    content: str = Field(
-        title="message content",
-    )
-    conversation: Optional["Conversation"] = Field(
-        Relationship(back_populates="messages", cascade_delete=True)
-    )
-    created_at: datetime = Field(
-        default_factory=datetime.now,
-        title="date and time the message was created",
     )
