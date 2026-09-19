ce0c84aa287c71a0e19def66726f3dcdcf31306f
surjanthakur <tsurjan506@gmail.com>
2026-09-19 15:51:13 +0530

    save

--- frontend/src/api/Client.api.js
+++ frontend/src/api/Client.api.js
@@ -1,18 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
 
-// const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
-// if (!BACKEND_URL) {
-//   throw new Error('VITE_BACKEND_URL is not defined')
-// }
+if (!BACKEND_URL) {
+  throw new Error('VITE_BACKEND_URL is not defined');
+}
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
-})
+});
 
 apiClient.interceptors.response.use(
   (response) => response,
@@ -20,26 +20,26 @@ apiClient.interceptors.response.use(
   (error) => {
     if (error.response) {
       // Backend ne response bheja
-      console.log('Status:', error.response.status)
-      console.log('Data:', error.response.data)
+      console.log('Status:', error.response.status);
+      console.log('Data:', error.response.data);
 
       if (error.response.status === 401) {
-        console.log('User not authenticated')
+        console.log('User not authenticated');
       }
 
       if (error.response.status === 500) {
-        console.log('Server error')
+        console.log('Server error');
       }
     } else if (error.request) {
       // Request gayi but response nahi aaya
-      console.log('Network error')
+      console.log('Network error');
     } else {
       // Request banane mein error
-      console.log(error.message)
+      console.log(error.message);
     }
 
-    return Promise.reject(error)
-  },
-)
+    return Promise.reject(error);
+  }
+);
 
-export default apiClient
+export default apiClient;
