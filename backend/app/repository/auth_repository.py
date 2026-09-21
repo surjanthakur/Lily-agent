028d07d64f7f61ef0d45924b3a44d6b411e66087
surjanthakur <tsurjan506@gmail.com>
2026-09-21 14:45:02 +0530

    svae

--- backend/app/repository/auth_repository.py
+++ backend/app/repository/auth_repository.py
@@ -16,32 +16,6 @@ async def get_user_by_google_id(google_id: str, session: AsyncSession):
     return result.one_or_none()
 
 
-async def update_oauth_account(
-    google_id: str,
-    access_token: str,
-    expires_at: datetime,
-    session: AsyncSession,
-):
-    statement = (
-        select(OAuthAccount)
-        .join(User, OAuthAccount.user_id == User.user_id)
-        .where(User.google_id == google_id)
-    )
-    result = await session.exec(statement)
-    oauth_account = result.one_or_none()
-
-    if not oauth_account:
-        return
-
-    oauth_account.access_token = access_token
-    oauth_account.expires_at = expires_at
-
-    await session.commit()
-    await session.refresh(oauth_account)
-
-    return oauth_account
-
-
 async def create_new_user(user: UserRequest, session: AsyncSession):
     new_user = User(
         google_id=user.google_id,
@@ -91,3 +65,29 @@ async def create_new_oauth_account(
     await session.refresh(new_oauth_account)
 
     return new_oauth_account
+
+
+async def update_oauth_account(
+    google_id: str,
+    access_token: str,
+    expires_at: datetime,
+    session: AsyncSession,
+):
+    statement = (
+        select(OAuthAccount)
+        .join(User, OAuthAccount.user_id == User.user_id)
+        .where(User.google_id == google_id)
+    )
+    result = await session.exec(statement)
+    oauth_account = result.one_or_none()
+
+    if not oauth_account:
+        return
+
+    oauth_account.access_token = access_token
+    oauth_account.expires_at = expires_at
+
+    await session.commit()
+    await session.refresh(oauth_account)
+
+    return oauth_account
