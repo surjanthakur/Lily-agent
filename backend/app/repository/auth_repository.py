23f1138e8cc35edc7290011632323d6d0d625377
surjanthakur <tsurjan506@gmail.com>
2026-09-21 14:44:18 +0530

    svae

--- backend/app/repository/auth_repository.py
+++ backend/app/repository/auth_repository.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from ..db.models import OAuthAccount, User
+from ..db.models import OAuthAccount, Session, User
 from ..schemas.user_req import UserRequest
 
 
@@ -54,14 +54,40 @@ async def create_new_user(user: UserRequest, session: AsyncSession):
     await session.commit(new_user)
     await session.refresh(new_user)
 
-    return new_user.username
+    return new_user
 
 
 async def create_new_session(
-    session: AsyncSession, user_id: UUID, expires_at: datetime
+    session: AsyncSession,
+    user_id: UUID,
+    expiry_date: datetime,
 ):
-    pass
+    new_session = Session(user_id=user_id, expires_at=expiry_date)
+    session.add(new_session)
+    await session.commit(new_session)
+    await session.refresh(new_session)
+
+    return new_session
 
 
-async def create_new_oauth_account(session: AsyncSession):
-    pass
+async def create_new_oauth_account(
+    session: AsyncSession,
+    user_id: UUID,
+    provider: str,
+    google_id: str,
+    access_token: str,
+    expiry_date: datetime,
+):
+    new_oauth_account = OAuthAccount(
+        user_id=user_id,
+        provider=provider,
+        provider_user_id=google_id,
+        access_token=access_token,
+        expires_at=expiry_date,
+    )
+
+    session.add(new_oauth_account)
+    await session.commit(new_oauth_account)
+    await session.refresh(new_oauth_account)
+
+    return new_oauth_account
